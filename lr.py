094e255284e00c16516e24ad87be94ecc60ee286
Ram81 <ramramrakhya81@gmail.com>
2017-01-27 14:19:33 +0530

    Cost Function & Gradient Descent Added

--- lr.py
+++ lr.py
@@ -1,6 +1,38 @@
 from numpy import loadtxt, zeros, ones, array, linspace, logspace
 from pylab import scatter, show, title, xlabel, ylabel, plot, contour
 
+#Evaluate Linear Regression
+def compute_Cost(X, Y, theta):
+	'''
+		Compute Cost function 
+	'''
+	m=Y.size
+	h_theta = X.dot(theta).flatten()
+	sqr_Error = (h_theta - y)**2
+	J = (1/(2*m)) * sqrError.sum()
+	return J
+	
+def gradient_Descent(X, Y, theta,alpha,iterations):
+	'''
+		Gradient Descent to learn theta by taking 
+		iterations gradient steps with learning rate alpha
+	'''
+	m = Y.size
+	J_history = zeros(shape=(iterations,1))
+	for i in range(iterations):
+		h_theta = X.dot(theta).flatten()
+		
+		error_x1 = (h_theta - y) * X[:,0]
+		error_x2 = (h_theta - y) * X[:,1]		
+		
+		theta[0] = theta[0] - alpha/m * error_x1.sum()
+		theta[1] = theta[1] - alpha/m * error_x2.sum()
+		
+		J_history[i,:] = compute_Cost(X,Y,theta)
+	
+	return theta, J_history
+
+
 #Load the Dataset
 data = loadtxt('Dataset/ex1data1.txt',delimiter=',')
 
@@ -16,7 +48,7 @@ X = data[:,0]
 Y = data[:,1]
 
 #number of traing examples
-m = X.size
+m = Y.size
 it = ones(shape=(m,2))
 
 it[:,1] = X
