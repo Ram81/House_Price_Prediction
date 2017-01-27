32c2cb460f601000f1a4d964e4b6e615417bbdcd
Ram81 <ramramrakhya81@gmail.com>
2017-01-27 14:07:27 +0530

    Paramters Loaded

--- lr.py
+++ lr.py
@@ -10,3 +10,22 @@ title('Profit Distribution')
 xlabel('Population in 10,000s')
 ylabel('Profit in $10,000s')
 show()
+
+#Fitting Linear Regression model
+X = data[:,0]
+Y = data[:,1]
+
+#number of traing examples
+m = X.size
+it = ones(shape=(m,2))
+
+it[:,1] = X
+
+#initialize theta parameter
+theta = zeros(shape=(2,1))
+
+#gradient Descent Setting
+iterations = 1500
+alpha = 0.01
+
+
