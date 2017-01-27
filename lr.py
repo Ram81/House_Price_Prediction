22921021d858fd78add4b9cde863b2a28dd58346
Ram81 <ramramrakhya81@gmail.com>
2017-01-27 14:35:41 +0530

    Prediction function Added

--- lr.py
+++ lr.py
@@ -8,8 +8,8 @@ def compute_Cost(X, Y, theta):
 	'''
 	m=Y.size
 	h_theta = X.dot(theta).flatten()
-	sqr_Error = (h_theta - y)**2
-	J = (1/(2*m)) * sqrError.sum()
+	sqr_Error = (h_theta - Y)**2
+	J = (1.0/(2*m)) * sqr_Error.sum()
 	return J
 	
 def gradient_Descent(X, Y, theta,alpha,iterations):
@@ -22,16 +22,19 @@ def gradient_Descent(X, Y, theta,alpha,iterations):
 	for i in range(iterations):
 		h_theta = X.dot(theta).flatten()
 		
-		error_x1 = (h_theta - y) * X[:,0]
-		error_x2 = (h_theta - y) * X[:,1]		
+		error_x1 = (h_theta - Y) * X[:,0]
+		error_x2 = (h_theta - Y) * X[:,1]		
 		
-		theta[0] = theta[0] - alpha/m * error_x1.sum()
-		theta[1] = theta[1] - alpha/m * error_x2.sum()
+		theta[0] = theta[0] - alpha * (1.0/m) * error_x1.sum()
+		theta[1] = theta[1] - alpha * (1.0/m) * error_x2.sum()
 		
 		J_history[i,:] = compute_Cost(X,Y,theta)
 	
 	return theta, J_history
 
+def Predict(X,theta):
+	predict = X.dot(theta).flatten()
+	return predict
 
 #Load the Dataset
 data = loadtxt('Dataset/ex1data1.txt',delimiter=',')
@@ -59,5 +62,36 @@ theta = zeros(shape=(2,1))
 #gradient Descent Setting
 iterations = 1500
 alpha = 0.01
+
+#compute & display initial cost
+print(compute_Cost(it,Y,theta))
+print()
+theta, J_History = gradient_Descent(it, Y, theta, alpha, iterations)
+
+print(theta)
+
+#predict values for population sizes 35,000 & 75,000
+
+X1 = array([1,3.5])
+X2 = array([1,7.5])
+
+print('For population of 35,000 we predict profit of %f' % (Predict(X1,theta) * 10000))
+print('For population of 75,000 we predict profit of %f' % (Predict(X2,theta) * 10000))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
